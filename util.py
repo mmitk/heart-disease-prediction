db9d82de9ef41bf62b12bfcc5e7fc582c48e4667
mmitk <51462992+mmitk@users.noreply.github.com>
2020-08-26 14:28:38 -0400

    util and dataExamination (initial)

---
+++ util.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+
+def plot_decision_function(X, y, clf, ax):
+    plot_step = 0.02
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+                         np.arange(y_min, y_max, plot_step))
+
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    ax.contourf(xx, yy, Z, alpha=0.4)
+    ax.scatter(X[:, 0], X[:, 1], alpha=0.8, c=y, edgecolor='k')
+
+def norm(df, target=None):
+    if target is not None:
+        y = df.pop(target)
+    x = df.values  # returns a numpy array
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    df = pd.DataFrame(x_scaled)
+    if target is not None:
+        df[target] = y
+    return df
