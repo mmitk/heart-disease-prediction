243caff23c4babd15052d41512a4701bff620906
mmitk <51462992+mmitk@users.noreply.github.com>
2020-08-28 10:41:12 -0400

    Baseline (no resampling) model trained and evaluated

---
+++ models.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import os
+import tempfile
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import sklearn
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+METRICS = [
+            keras.metrics.TruePositives(name='tp'),
+            keras.metrics.FalsePositives(name='fp'),
+            keras.metrics.TrueNegatives(name='tn'),
+            keras.metrics.FalseNegatives(name='fn'),
+            keras.metrics.BinaryAccuracy(name='accuracy'),
+            keras.metrics.Precision(name='precision'),
+            keras.metrics.Recall(name='recall'),
+            keras.metrics.AUC(name='auc'),
+        ]
+
+class modelObj:
+
+    def __init__(self, n_cols=None):
+        self.EPOCHS = 100
+        self.BATCH_SIZE = 32
+
+        if n_cols is not None:
+            self.create_model(n_cols)
+        else:
+            self.model = None
+
+    def create_model(self, n_cols, metrics=METRICS, bias=None):
+        model = keras.Sequential([
+            keras.layers.Dense(
+                16, activation='relu',
+                input_shape=(n_cols,)),
+            keras.layers.Dropout(0.5),
+            keras.layers.Dense(1, activation='sigmoid',bias_initializer=bias),
+        ])
+
+        model.compile(
+            optimizer=keras.optimizers.Adam(lr=1e-3),
+            loss=keras.losses.BinaryCrossentropy(),
+            metrics=metrics)
+
+        self.model = model
+        return model
+
+    def get_summary(self):
+        if self.model is not None:
+            return self.model.summary()
+        else:
+            return 'Model is NoneType'
+
+
+class printDot(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs):
+        if epoch % 10 == 0: print('.')
--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
+import matplotlib.pyplot as plt
 
 def plot_decision_function(X, y, clf, ax):
     plot_step = 0.02
@@ -24,3 +25,23 @@ def norm(df, target=None):
     if target is not None:
         df[target] = y
     return df
+
+def plot_metrics(history):
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    metrics =  ['loss', 'auc', 'precision', 'recall']
+    for n, metric in enumerate(metrics):
+        name = metric.replace("_"," ").capitalize()
+        plt.subplot(2,2,n+1)
+        plt.plot(history.epoch,  history.history[metric], color=colors[0], label='Train')
+        plt.plot(history.epoch, history.history['val_'+metric],
+             color=colors[0], linestyle="--", label='Val')
+        plt.xlabel('Epoch')
+        plt.ylabel(name)
+        if metric == 'loss':
+            plt.ylim([0, plt.ylim()[1]])
+        elif metric == 'auc':
+            plt.ylim([0.8,1])
+        else:
+            plt.ylim([0,1])
+
+        plt.legend()
